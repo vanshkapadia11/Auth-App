1a82bbb1b1c4ed3ac6a38ce31cf67a663647c58e
Vansh Kapadia <144871405+vanshkapadia11@users.noreply.github.com>
2025-07-15 11:05:14 +0530

    Added Some More Functionality!!

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const { user } = useAuth();
   const [isDark, setIsDark] = useState(() => {
     return localStorage.theme === "dark";
@@ -31,12 +33,22 @@ const Navbar = () => {
               <ul className="flex items-center text-sm font-semibold space-x-4 justify-center ring-1 ring-inset dark:bg-[#242424] dark:ring-[#2a2a2a] ring-[#e8e8e8] bg-[#f9f9f9] transition-all py-2 px-4 rounded-xl">
                 {user ? (
                   <>
-                    <li className="uppercase">home</li>
+                    <li className="uppercase cursor-pointer">home</li>
                   </>
                 ) : (
                   <>
-                    <li className="uppercase">signup</li>
-                    <li className="uppercase">login</li>
+                    <li
+                      className="uppercase cursor-pointer"
+                      onClick={() => navigate("/signup")}
+                    >
+                      signup
+                    </li>
+                    <li
+                      className="uppercase cursor-pointer"
+                      onClick={() => navigate("/login")}
+                    >
+                      login
+                    </li>
                   </>
                 )}
                 <li className="link">
